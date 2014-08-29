550bc58621102784f1d704f55db0856802f996cc
gonzaldd <gonzalodedomingo@gmail.com>
2014-08-29 16:02:08 -0300

    Add

--- web/settings.py
+++ web/settings.py
@@ -64,7 +64,7 @@ TEMPLATE_DIRS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'sitedb.db'),
     }
 }
 
