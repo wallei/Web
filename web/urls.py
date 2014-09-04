c31337cd0b4813bec96773746fd8de16f5a677cb
sebacasca <sebacasca@outlook.com>
2014-09-03 21:19:45 -0300

    Update urls.py

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'web.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$','commerce.views.lista_producto'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
